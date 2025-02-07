0380bfd4e9df2f184881d5211c31d6e8f9e071e1
googledisable <claudiovigil2491981gsy@gmail.com>
2025-02-08 02:40:49 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Google has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1(888) 295-0630 to unlock it!';
+defaultText = 'Your Google has been Locked due to Detected illegal Child Pornography. Your Google Account has been disabled. Immediately Call Online Security Support +1(888) 295-0630 to Unlock it!';
